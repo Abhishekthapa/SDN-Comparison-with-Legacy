ec647c7cfc31de6b948a31d1b13e829d68a42f24
Roshan Guragain <guragainroshan0@gmail.com>
2021-07-03 04:43:48 -0700

    changes made in vary_packet

---
+++ sdnip/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo mn --custom /home/sdnip/major/MajorProject/sdnip/final.py --topo sdnip --controller remote,10.0.3.11
--- vary_packet.sh
+++ vary_packet.sh
@@ -4,14 +4,19 @@
 dest=$1
 if [[ $# -ne 3 ]] 
 then
-   echo "Usage : h1_ping.sh <destination_IP> <folder_name> <file_name>"
+   echo "Usage : vary_packet.sh <destination_IP> <folder_name> <file_name>"
    exit 2
 fi
-for i in 5 10 20 30 50 100
+for i in $(seq 5 5 100)
 do
   if [ ! -d ./legacy_data/ping/$2 ]
   then
-	mkdir ./legacy_data/ping/$2
+	mkdir -p ./legacy_data/ping/$2
   fi
-  ping $dest -c $i | tee ./legacy_data/ping/$2/$3_$i
+  
+  mdevrtt=$(ping $dest -c $i | tee ./legacy_data/ping/$2/$3_$i | grep "mdev" | cut -d '/' -f 7 | cut -d " " -f 1)
+  date=$(date)
+  echo "$date $2 $i : $mdevrtt" >> rtt_data
+  tail -n 1 rtt_data 
+
 done
